560940642da39f8b924fd9a3eda97e2d158a9ba4
Jason Goldfarb <yosegold@gmail.com>
2015-08-05 21:04:56 -0400

    Abbreviated State names, and phantom locations are gone!

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,7 +6,7 @@
          "email": "jason.r.goldfarb@gmail.com",
          "github": "jgoldfarb",
          "twitter": "jasonrgoldfarb",
-         "location": "New York City Metropolitan Area",
+         "location": "New York, NY",
        },
       "welcomeMessage": "This is my Front-End Web Development resume",
       "skills": ["Web Development", "Six Sigma Green Belt", "Project Management", "Digital & Social Media", "Online Technology"],
@@ -61,35 +61,35 @@
         {
         "employer": "AT&T Wireless",
         "title": "LTE Site Acquisition Project Manager",
-        "location": "Huntington, New York",
+        "location": "Huntington, NY",
         "dates": "November 2013 - Present",
         "description": "Manage a team of project managers and multiple vendors covering the site acquisition process for the Carrier Add upgrades to AT&T’s LTE network in the New York City metropolitan area and northern New Jersey. Completed 100% of the 2014 LTE upgrade projects for 800+ locations, ahead of time, and within budget. Lead weekly project development and deployment meetings with multiple site acquisition and civil project vendors.	Provide guidance for complicated, legal, technical, business, and contractual issues that impact project timelines. Create market-specific master trackers, and provide in depth reporting of all milestone progress to senior level executive teams. Responsible for maintaining project schedules and for hitting and exceeding goals linked to planned, forecasted, and completion objectives. On board new vendor teams, and liaison with all internal and external groups accountable for LTE deployment in the market.",
       },
       {
         "employer": "AT&T Wireless",
         "title": "Senior Real Estate and Construction Manager",
-        "location": "Huntington, New York",
+        "location": "Huntington, NY",
         "dates": "October 2011 - November 2013",
         "description": "Recipient of the “Top 5 Idea Award” from The Innovation Pipeline IT Innovation Program. Awarded Six Sigma Green Belt Certification.	Assembled and led a team that conducted due diligence, lease reviews, and amendment responsibilities for national projects impacting the local market.	Saved $150,000+ in operating expenses by avoiding costs associated with temporary equipment moves, and negotiating cost-cutting departure agreements. Principal advocate and contributor of a Six Sigma/Lean project designed reduce access issues and improve real estate relationships. Cross collaborated with executive management, corporate and outside counsel, and numerous internal and external teams, as the subject matter expert and point of contact for multiple projects/issues affecting the wireless real estate portfolio – including, but not limited to site access, construction, new site builds, temporary equipment moves, equipment upgrades (UMTS, LTE, 4G, HVAC, Camlock, Generators, etc.), relocations, and terminations. Responsible for the review and negotiation of business and contract terms for new lease agreements and amendments.",
       },
       {
         "employer": "T-Mobile USA",
         "title": "Property Specialist",
-        "location": "Great River, New York",
+        "location": "Great River, NY",
         "dates": "2008 - October 2011",
         "description": "Managed a portfolio of thousands of real estate lease agreements and assets, representing every cell site location across the New York City and Long Island markets. Served as the central subject matter resource on all contract details for cross-functional internal teams, providing in-depth knowledge of lease terms and legalities to ensure seamless and continuous delivery of cellular service. Negotiated with and managed relationships with property owners to promote the most advantageous business position for the portfolio.Turned around the portfolio’s financial position, building a system lauded as a company-wide model for portfolio management, by conducting a complete portfolio risk assessment and structural overhaul. Delivered more than $1.7 million in direct operating expense savings, a figure which was projected to translate into more than $13 million in long-term dollar savings, by identifying and resolving a wide variety of discrepancies and overpayments, recommending the elimination of unnecessary or duplicate sites, and negotiating more favorable contract terms.Launched a successful effort to eliminate an entire category of “at risk” contract documents, establishing a clear process for document retention, and training junior staffers on identifying and correcting a tremendous backlog of document issues. Played a leadership role in increasing the number of fully executed new agreements by 500% as project manager of the end-to-end leasing process with an external site acquisition firm.Consistently recognized by senior executive leadership as a top performer within the property management team, setting the precedent and establishing benchmarks for property management excellence for the nationwide operation.",
       },
       {
         "employer": "Independent Contractor",
         "title": "Wireless Leasing & Real Estate Consultant ",
-        "location": "Woodmere, New York",
+        "location": "Woodmere, NY",
         "dates": "2007- 2008",
         "description": "Provided specialized expertise in wireless lease acquisitions and sales, as well as advisory services for the real estate industry. Conducted in-depth research and due diligence to advise clients with cost-benefit and risk analyses and to recommend optimal deal structures.Served as the legal consultant for a real estate investor client, ensuring proper vetting and preparation of all potential deals.Orchestrated an auction for more than 100 properties, coordinating the efforts of multiple owners to compile and present detailed property information, schedule open houses, and advertise for the event.	Created strong legal protections for a client’s business interests by re-drafting the base contract document.",
       },
       {
         "employer": "Unison Site Management",
         "title": "Vice President of Origination",
-        "location": "New York, New York",
+        "location": "New York, NY",
         "dates": "2005 - 2007",
         "description": "Directed a 15-member sales and legal team in the oversight of the complete site acquisition process, from initiation through closure. Concurrently managed up to 80 active deals at all stages of the approval process, navigating and problem solving diverse and complex issues to drive a high percentage of deals to the closing table. Served as the company’s central subject matter expert and go-to business leader for communicating issues between the sales and legal departments, ensuring full understanding of the intricacies of all sides of the deal process.	Played a critical leadership role within the organization, leveraging a deep background both as an attorney and in sales to build the firm’s cross-functional and often competing departments into a cohesive, high-performance operation. Dramatically increased the rate of deal closings while delivering 10% cost savings and slashing origination times by re-engineering the complete acquisition processes to ensure that the highest quality deals entered the origination cycle.Boosted team morale and productivity with this new system that more directly linked performance to compensation.Built this team into the company’s top performing entity representing one-third of all closed business and more than $40 million in paid proceeds. Recognized for outstanding results and origination leadership with selection to train the entire company in the newly designed origination process.",
       }
@@ -128,7 +128,7 @@
       "schools": [
     	   {
     		  "name": "Hofstra University",
-    	    "location": "Hempstead, New York",
+    	    "location": "Hempstead, NY",
     	    "degree" : "Continuing Education",
     	    "majors": ["Digital & Social Media Marketing"],
     	    "dates": 2011,
@@ -136,7 +136,7 @@
           },
         {
           "name": "Brooklyn Law School",
-          "location": "Brooklyn, New York",
+          "location": "Brooklyn, NY",
           "degree" : "Juris Doctor",
           "majors": ["Law"],
           "dates": 1994,
@@ -144,7 +144,7 @@
           },
         {
           "name": "Yeshiva Univeristy - Sy Syms School of Business",
-          "location": "New York, New York",
+          "location": "New York, NY",
           "degree" : "Bachelor of Science",
           "majors": ["Finance"],
           "dates": 1992,
